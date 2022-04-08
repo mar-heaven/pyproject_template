6e5288b1369899630806a9b3e4924e434de436bb
Ginta <775650117@qq.com>
2022-04-08 16:35:03 +0800

    :bug: FIX rm folder

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -8,9 +9,12 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+def remove_folder(folder_path):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, folder_path))
+
 
 if __name__ == '__main__':
     if '{{ cookiecutter.is_service }}' != 'y':
         remove_file('Dockerfile')
     if '{{ cookiecutter.is_opensource }}' != 'y':
-        remove_file('.github')
+        remove_folder('.github')
